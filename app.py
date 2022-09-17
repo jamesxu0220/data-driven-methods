513cc2e5f70a3090a18db5811547ca0ba693ea49
James Xu <zx2370@columbia.edu>
2022-09-17 01:02:44 -0400

    Updated Performance-RPS

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-## ------------------------------------------------------------------
-## ------------------------------------------------------------------
-## Library Imports
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+# Library Imports
 
 from tkinter.messagebox import RETRY
-import os, csv
+import os
+import csv
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import numpy as np
 
-## ------------------------------------------------------------------
-## ------------------------------------------------------------------
-## App configurations
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+# App configurations
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['UPLOAD_FOLDER'] = './______submissions/'
 db = SQLAlchemy(app)
 
-## ------------------------------------------------------------------
-## ------------------------------------------------------------------
-## Data Model
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+# Data Model
+
 
 class Group(db.Model):
     group_id = db.Column(db.Integer, primary_key=True)
@@ -31,42 +33,45 @@ class Group(db.Model):
     member4_id = db.Column(db.String(15))
     member5_id = db.Column(db.String(15))
 
+
 class Decision(db.Model):
     group_id = db.Column(db.Integer, primary_key=True)
     submission_id = db.Column(db.Integer, primary_key=True)
     symbol = db.Column(db.String(10), primary_key=True)
-    rank1 =  db.Column(db.Float, nullable=False)
-    rank2 =  db.Column(db.Float, nullable=False)
-    rank3 =  db.Column(db.Float, nullable=False)
-    rank4 =  db.Column(db.Float, nullable=False)
-    rank5 =  db.Column(db.Float, nullable=False)
-    decision =  db.Column(db.Float, nullable=False)
+    rank1 = db.Column(db.Float, nullable=False)
+    rank2 = db.Column(db.Float, nullable=False)
+    rank3 = db.Column(db.Float, nullable=False)
+    rank4 = db.Column(db.Float, nullable=False)
+    rank5 = db.Column(db.Float, nullable=False)
+    decision = db.Column(db.Float, nullable=False)
+
 
 class Performance(db.Model):
-     submission_id = db.Column(db.Integer, primary_key=True)
-     group_id = db.Column(db.Integer, primary_key=True)
-     rps = db.Column(db.Float)
-     #ret = db.Column(db.Float)
-     #sdp = db.Column(db.Float)
-     #ir = db.Column(db.Float)
-     #rps_rank = db.Column(db.Integer)
-     #ir_rank = db.Column(db.Integer)
-     #overall_ranking = db.Column(db.Integer)
+    submission_id = db.Column(db.Integer, primary_key=True)
+    group_id = db.Column(db.Integer, primary_key=True)
+    rps = db.Column(db.Float)
+    #ret = db.Column(db.Float)
+    #sdp = db.Column(db.Float)
+    #ir = db.Column(db.Float)
+    #rps_rank = db.Column(db.Integer)
+    #ir_rank = db.Column(db.Integer)
+    #overall_ranking = db.Column(db.Integer)
 
 
-## ------------------------------------------------------------------
-## ------------------------------------------------------------------
-## Pages
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+# Pages
 
-## ------------------------------------------------------------------
-## Homepage
+# ------------------------------------------------------------------
+# Homepage
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-## ------------------------------------------------------------------
-## Group Tab
+# ------------------------------------------------------------------
+# Group Tab
+
 
 @app.route('/groups/', methods=['POST', 'GET'])
 def groups():
@@ -85,7 +90,8 @@ def groups():
         m3_id = request.form['member3_id']
         m4_id = request.form['member4_id']
         m5_id = request.form['member5_id']
-        new_group = Group(group_id=gid, group_name=gname, member1_id=m1_id, member2_id=m2_id, member3_id=m3_id, member4_id=m4_id, member5_id=m5_id)
+        new_group = Group(group_id=gid, group_name=gname, member1_id=m1_id,
+                          member2_id=m2_id, member3_id=m3_id, member4_id=m4_id, member5_id=m5_id)
         try:
             db.session.add(new_group)
             db.session.commit()
@@ -95,12 +101,14 @@ def groups():
     else:
         groups = Group.query.order_by(Group.group_id).all()
         return render_template('groups.html', groups=groups)
-        
-## ------------------------------------------------------------------
-## Submission Tab
 
-def allowed_file(filename): # checks to make sure submission file is a csv file
+# ------------------------------------------------------------------
+# Submission Tab
+
+
+def allowed_file(filename):  # checks to make sure submission file is a csv file
     return '.' in filename and filename.rsplit('.', 1)[1].lower() == "csv"
+
 
 @app.route('/submit/', methods=['POST', 'GET'])
 def submit():
@@ -115,7 +123,8 @@ def submit():
                 """
         sid = request.form['submission_id']
         if Decision.query.filter_by(group_id=gid).filter_by(submission_id=sid).all():
-            all_prev = Decision.query.filter_by(group_id=gid).filter_by(submission_id=sid).all()
+            all_prev = Decision.query.filter_by(
+                group_id=gid).filter_by(submission_id=sid).all()
             for record in all_prev:
                 db.session.delete(record)
             db.session.commit()
@@ -135,8 +144,8 @@ def submit():
                     if round(float(row['rank1']) + float(row['rank2']) + float(row['rank3']) + float(row['rank4']) + float(row['rank5']), 3) != 1:
                         return "Error: " + str(row["id"]) + "'s ranks do not sum to unity"
                     total_weight += float(row['decision'])
-                    decisions.append(Decision(group_id=gid, submission_id=sid, symbol=row['id'], rank1=row['rank1'], \
-                        rank2=row['rank2'], rank3=row['rank3'], rank4=row['rank4'], rank5=row['rank5'], decision = row['decision']))
+                    decisions.append(Decision(group_id=gid, submission_id=sid, symbol=row['id'], rank1=row['rank1'],
+                                              rank2=row['rank2'], rank3=row['rank3'], rank4=row['rank4'], rank5=row['rank5'], decision=row['decision']))
                 if len(decisions) != 110:
                     return """
                         <!doctype html>
@@ -158,6 +167,8 @@ def submit():
                             db.session.commit()
                         except:
                             return "Error: There was an issue adding data for stock " + str(decision.symbol)
+                this_file.close()
+                os.remove(this_file.name)
             if erased:
                 return """
                     <!doctype html>
@@ -183,15 +194,17 @@ def submit():
     else:
         return render_template('submit.html')
 
-## ------------------------------------------------------------------
-## Portfolio Tabs
+# ------------------------------------------------------------------
+# Portfolio Tabs
+
 
 @app.route('/portfolios/', methods=['POST', 'GET'])
 def portfolios():
-    if request.method == 'POST':    
+    if request.method == 'POST':
         gid = request.form['group_id']
         sid = request.form['submission_id']
-        portfolios = Decision.query.filter_by(group_id=gid).filter_by(submission_id=sid).all()
+        portfolios = Decision.query.filter_by(
+            group_id=gid).filter_by(submission_id=sid).all()
         if len(portfolios) == 0:
             return """
             <!doctype html>
@@ -203,44 +216,53 @@ def portfolios():
     else:
         return render_template('portfolio_inquiry.html')
 
-## ------------------------------------------------------------------
-## Performance Tab
-def getRPS(gid: int, sid: int):
+# ------------------------------------------------------------------
+# Performance Tab
+
+
+def getRPS(gid: int, sid: int) -> float:
     try:
-        portfolio = Decision.query.filter_by(group_id=gid).filter_by(submission_id=sid).first()
-        sample = Decision.query.filter_by(group_id=-999).filter_by(submission_id=sid).first()
+        portfolio = Decision.query.filter_by(
+            group_id=gid).filter_by(submission_id=sid).first()
+        sample = Decision.query.filter_by(
+            group_id=-999).filter_by(submission_id=sid).first()
     except:
         return "Unexpected error in getRPS"
-    f_array = np.array([[portfolio.rank1, portfolio.rank2, portfolio.rank3, portfolio.rank4, portfolio.rank5]])
+    f_array = np.array([[portfolio.rank1, portfolio.rank2,
+                       portfolio.rank3, portfolio.rank4, portfolio.rank5]])
     f = np.cumsum(f_array, axis=1)
-    q_array = np.array([[sample.rank1, sample.rank2, sample.rank3, sample.rank4, sample.rank5]])
+    q_array = np.array(
+        [[sample.rank1, sample.rank2, sample.rank3, sample.rank4, sample.rank5]])
     q = np.cumsum(q_array, axis=1)
-    return float(np.sum((q-f)**2,axis=1)/f.shape[1])
+    return float(np.sum((q-f)**2, axis=1)/f.shape[1])
+
 
 @app.route('/performance/', methods=['POST', 'GET'])
 def performance():
     if request.method == 'POST':
         gid = request.form['group_id']
         sid = request.form['submission_id']
-        try:
-            performance = Performance.query.filter_by(group_id=gid).filter_by(submission_id=sid).first()
-        except:
+        perf = Performance.query.filter_by(
+            group_id=gid).filter_by(submission_id=sid).first()
+        if perf is None:
             new_rps = getRPS(gid, sid)
-            new_perf = Performance(group_id=gid, submission_id=sid, rps=new_rps)
+            new_perf = Performance(
+                group_id=gid, submission_id=sid, rps=new_rps)
             try:
                 db.session.add(new_perf)
                 db.session.commit()
             except:
-                return "error adding new perf"
-            performance = Performance.query.filter_by(group_id=gid).filter_by(submission_id=sid).first()
-            return render_template('performance.html', data=performance)
+                return "Error adding new performance result to db"
+        performance = Performance.query.filter_by(
+            group_id=gid).filter_by(submission_id=sid).first()
+        return render_template('performance.html', performance=performance)
     else:
         return render_template('performance_inquiry.html')
 
 
-## ------------------------------------------------------------------
-## ------------------------------------------------------------------
-## Ranking Tab
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+# Ranking Tab
 
 
 if __name__ == "__main__":
