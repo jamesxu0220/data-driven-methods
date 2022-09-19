f5cdd032f6395b405e1d196700f81ab2ab34493f
James Xu <zx2370@columbia.edu>
2022-09-19 13:54:01 -0400

    Overhaul performance section

--- DataDrivenFinance/performance.py
+++ DataDrivenFinance/performance.py
@@ -1,6 +1,6 @@
-from DataDrivenFinance import app, get_yfinance
-from DataDrivenFinance.databases import db, Decision, Performance, TradingWeek, ActualRanks, ActualPrices
-from flask import request, render_template
+from DataDrivenFinance import app
+from DataDrivenFinance.databases import Group, Decision, ActualRanks, ActualPrices, Performance
+from flask import render_template
 import numpy as np
 
 
@@ -38,39 +38,13 @@ def getIR(gid: int, sid: int) -> list[float]:
     return [float(ret), float(std), float(ir)]
 
 
-@app.route('/performance/', methods=['POST', 'GET'])
+@app.route('/performance/')
 def performance():
-    if request.method == 'POST':
-        gid = request.form['group_id']
-        sid = request.form['submission_id']
-
-        if not Decision.query.filter_by(
-                group_id=gid, submission_id=sid).all():
-            return "Error: portfolio does not exist"
-
-        elif not TradingWeek.query.filter_by(submission_id=sid).all():
-            return "Error: Results not ready for week " + str(sid)
-
-        elif not ActualRanks.query.filter_by(submission_id=sid).all():
-            # Maybe add a progress bar here???
-            # Grabs new info from yahoo finance and calculates the actual prices/ranks
-            get_yfinance.calculateRanks(sid)
-
-        performance = Performance.query.filter_by(
-            group_id=gid, submission_id=sid).first()
-
-        if performance is None:
-            new_rps = getRPS(gid, sid)
-            new_ret, new_sdp, new_ir = getIR(gid, sid)
-            new_perf = Performance(
-                group_id=gid, submission_id=sid, rps=new_rps, ret=new_ret, sdp=new_sdp, ir=new_ir)
-            try:
-                db.session.add(new_perf)
-                db.session.commit()
-            except:
-                return "Error when adding new performance result to database"
-            performance = Performance.query.filter_by(
-                group_id=gid, submission_id=sid).first()
-        return render_template('performance/performance.html', performance=performance)
-    else:
-        return render_template('performance/performance_inquiry.html')
+    perfs = Performance.query.order_by(
+        Performance.submission_id, Performance.group_id).all()
+    performances = []
+    for perf in perfs:
+        gname = Group.query.filter_by(
+            group_id=perf.group_id).first().group_name
+        performances.append([perf, gname])
+    return render_template('performance.html', performances=performances)
--- DataDrivenFinance/test.py
+++ DataDrivenFinance/test.py
@@ -1,24 +1,36 @@
-# FOR DEBUGGING ONLY
-# FOR DEBUGGING ONLY
-# FOR DEBUGGING ONLY
-
-from DataDrivenFinance import app, get_yfinance
-from DataDrivenFinance.databases import db, TradingWeek
-
-@app.route('/test/get_ranks/')
-def get_ranks_test():
-    get_yfinance.calculateRanks(0)
-    return "Success"
+from DataDrivenFinance import app, get_yfinance, performance
+from DataDrivenFinance.databases import db, Decision, Performance, TradingWeek, ActualRanks
 
 
+@app.route('/test/set_week/<int:sid>/<string:start_date>/<string:end_date>')
 def set_new_week(sid, start_date, end_date):
+    start_date = "2022-" + start_date
+    end_date = "2022-" + end_date
+    # first week 0 - "2022-09-09" to "2022-09-17"
+    # so link to update is /test/set_week/0/09-09/09-17
+
     new_week = TradingWeek(
         submission_id=sid, start_day=start_date, end_day=end_date)
     db.session.add(new_week)
     db.session.commit()
 
+    #if not ActualRanks.query.filter_by(submission_id=sid).all():
+    get_yfinance.calculateRanks(sid)
 
-@app.route('/test/set_week/')
-def set_week_test():
-    set_new_week(0, "2022-09-09", "2022-09-17")
-    return "Success"
+    gids = Decision.query.filter_by(submission_id=sid).with_entities(
+        Decision.group_id).distinct().all()
+
+    for gid in gids:
+        gid = gid.group_id
+        new_rps = performance.getRPS(gid, sid)
+        new_ret, new_sdp, new_ir = performance.getIR(gid, sid)
+        new_perf = Performance(
+            group_id=gid, submission_id=sid, rps=new_rps, ret=new_ret, sdp=new_sdp, ir=new_ir)
+        try:
+            db.session.add(new_perf)
+            db.session.commit()
+        except:
+            return "Error when adding new performance result to database for group " + str(gid)
+
+    return "Success setting up results for week " + str(sid) + "; added performance results for " + \
+        str(len(gids)) + " groups."
